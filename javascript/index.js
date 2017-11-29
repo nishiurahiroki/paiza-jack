87b94c1c35d1de6112e94fd9b784bb50cf5494aa
nishiurahiroki <nishiurahiroki@gmail.com>
2017-11-29 22:29:31 +0900

    refactoring.

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,27 @@
+const BET_CHIP = '1';
+
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 let inputData = '';
-
 process.stdin.on('data', (chunk) => {
-    inputData += chunk;
+  inputData += chunk;
 });
 
 process.stdin.on('end', () => {
-    var lines = inputData.split('\n');
-    var line_1 = lines[0];
-    my_card = line_1.split(" ");
+  const myCards = inputData
+                    .split('\n')[0]
+                    .split(' ')
 
-    if(my_card[0] == "0"){
-        console.log("1"); // 賭けチップ数
+  const isChipBetTurn = '0' === myCards[0]
+  if(isChipBetTurn){
+    console.log(BET_CHIP); // 賭けチップ数
+    return;
+  }
 
-    }else{
-        var card_total = 0;
-        for(var i=0; i<my_card.length; i++){
-            card_total += parseInt(my_card[i]);
-        }
-        if(card_total < 10){ //★カードを引く条件の合計値を変えてみよう！★
-            console.log("HIT");// カード引く
-        }else{
-            console.log("STAND");// 勝負
-        }
-    }
+  const total = myCards.reduce((previous, current) => (+previous) + (+current))
+  if(total < 10){ //★カードを引く条件の合計値を変えてみよう！★
+    console.log("HIT");// カード引く
+  }else{
+    console.log("STAND");// 勝負
+  }
 });
